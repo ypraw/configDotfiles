ae266b6cef728b6db1dccfa078bc58a6e823aae7
ypraw <dyo.ndutz@gmail.com>
2020-07-21 14:28:00 +0700

    change color bars for glava

--- glava/bars.glsl
+++ glava/bars.glsl
@@ -7,19 +7,19 @@
 /* Width (in pixels) of each bar gap */
 #define BAR_GAP 2
 /* Outline color */
-#define BAR_OUTLINE #f0a6a7
+#define BAR_OUTLINE #029aa5
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 125
+#define AMPLIFY 200
 /* Alpha channel for bars color */
 #define ALPHA 0.8
 /* How strong the gradient changes */
-#define GRADIENT_POWER 50
+#define GRADIENT_POWER 100
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 1)
 /* Bar color */
-#define COLOR (#7060eb * GRADIENT * ALPHA)
+#define COLOR (#fe6637 * GRADIENT * ALPHA)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
