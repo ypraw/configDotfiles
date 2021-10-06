d5037cd42b189cce451f4e626886075e40dcef5c
ypraw <dyo.ndutz@gmail.com>
2021-10-06 13:15:23 +0700

    CHORE(glava): Changed bars color

--- glava/.config/glava/bars.glsl
+++ glava/.config/glava/bars.glsl
@@ -7,7 +7,7 @@
 /* Width (in pixels) of each bar gap */
 #define BAR_GAP 2
 /* Outline color */
-#define BAR_OUTLINE #e099bd
+#define BAR_OUTLINE #b16286
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
@@ -19,7 +19,7 @@
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 0.8)
 /* Bar color */
-#define COLOR (#e099bd * GRADIENT * ALPHA)
+#define COLOR (#b16286 * GRADIENT * ALPHA)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
