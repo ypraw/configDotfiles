858f4fe0a4cd4e046fb507841007761d6f0cfd23
ypraw <yunindyo.prabowo@gmail.com>
2022-02-17 15:16:53 +0700

    Updated source

--- glava/.config/glava/bars.glsl
+++ glava/.config/glava/bars.glsl
@@ -7,7 +7,7 @@
 /* Width (in pixels) of each bar gap */
 #define BAR_GAP 2
 /* Outline color */
-#define BAR_OUTLINE #b16286
+#define BAR_OUTLINE #d79921
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
@@ -19,7 +19,7 @@
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 0.8)
 /* Bar color */
-#define COLOR (#b16286 * GRADIENT * ALPHA)
+#define COLOR (#d79921 * GRADIENT * ALPHA)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
